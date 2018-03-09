632aca2d4c4b747f6da7b17b8e931f4341383f5b
fegomes <nandogsilva@hotmail.com>
2018-03-08 22:19:43 -0300

    libhello initial commit

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+//
+// Created by @fegomes on 08/03/18.
+//
+
+#include "hello.hpp"
+
+Hello::Hello() {
+
+}
+
+Hello::~Hello() {
+
+}
+
+std::string Hello::to_string() {
+    return "Hello World!";
+}
---
+++ hello.hpp
@@ -0,0 +1,16 @@
+//
+// Created by @fegomes on 08/03/18.
+//
+
+#ifndef LIBHELLO_HELLO_HPP
+#define LIBHELLO_HELLO_HPP
+#include <string>
+
+class Hello {
+public:
+    Hello();
+    ~Hello();
+public:
+    static std::string to_string();
+};
+#endif //LIBHELLO_HELLO_HPP
